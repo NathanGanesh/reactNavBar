ef98baa9f648674ecbe47552491eb8a5a8cad904
nathan ganesh <489290@student.saxion.nl>
2020-11-14 15:20:44 +0100

    final push

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,29 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+	return (
+		<nav className="nav-bar">
+			<img src={logo} />
+			<ul className="main-links">
+				{links.map((item) => {
+					return <li>{item.text}</li>;
+				})}
+			</ul>
+			<ul className="main-logo">
+				{social.map((item) => {
+					return (
+						<li>
+							<a href={item.url} />
+							{item.icon}
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
